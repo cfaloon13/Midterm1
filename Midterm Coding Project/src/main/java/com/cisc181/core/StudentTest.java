673c51f10f73917c2ff88130ee91462890f608ad
Connor <cfaloon@udel.edu>
2016-03-20 21:08:26 -0400

    hii

---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/StudentTest.java
@@ -0,0 +1,39 @@
+package com.cisc181.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentTest {
+
+	@Test
+	public void testPrintName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMajor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMajor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStudentID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintNameBoolean() {
+		fail("Not yet implemented");
+	}
+
+}
